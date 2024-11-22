f0cac5a44020f9f2008f7e4ea236ed70077c4b85
Kalindu Sudaraka <146382563+Kalindusu@users.noreply.github.com>
2024-11-22 22:59:54 +0530

    JWTAuthFilter class update

--- backend/src/main/java/com/example/usermanagementsystem/config/JWTAuthFilter.java
+++ backend/src/main/java/com/example/usermanagementsystem/config/JWTAuthFilter.java
@@ -7,10 +7,13 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.security.Security;
 
 @Component
 public class JWTAuthFilter extends OncePerRequestFilter {
@@ -33,5 +36,8 @@ public class JWTAuthFilter extends OncePerRequestFilter {
 
         jwtToken= autheHeader.substring(7);
         userEmail = jwtUtils.extractUsername(jwtToken);
+        if(userEmail != null && SecurityContextHolder.getContext().getAuthentication()==null){
+            UserDetails userDetails = ourUserDetailsService.loadUserByUsername(userEmail);
+        }
     }
 }

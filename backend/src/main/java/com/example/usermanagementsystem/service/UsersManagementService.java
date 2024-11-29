95aab4026c7d17cb472ff412b9cd261b358abefd
Kalindu Sudaraka <146382563+Kalindusu@users.noreply.github.com>
2024-11-29 22:30:52 +0530

    OurUserDetailsService class update

--- backend/src/main/java/com/example/usermanagementsystem/service/UsersManagementService.java
+++ backend/src/main/java/com/example/usermanagementsystem/service/UsersManagementService.java
@@ -1,7 +1,12 @@
 package com.example.usermanagementsystem.service;
 
+import com.example.usermanagementsystem.repository.UsersRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UsersManagementService {
+
+    @Autowired
+    private UsersRepo usersRepo;
 }

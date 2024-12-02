8cca252cab19089fd243f262cedfe4590b519f4f
Kalindu Sudaraka <146382563+Kalindusu@users.noreply.github.com>
2024-12-03 00:38:47 +0530

    OurUsersManagementService class update

--- backend/src/main/java/com/example/usermanagementsystem/service/UsersManagementService.java
+++ backend/src/main/java/com/example/usermanagementsystem/service/UsersManagementService.java
@@ -1,7 +1,10 @@
 package com.example.usermanagementsystem.service;
 
+import com.example.usermanagementsystem.dto.ReqRes;
 import com.example.usermanagementsystem.repository.UsersRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,4 +15,13 @@ public class UsersManagementService {
 
     @Autowired
     private JWTUtils jwtUtils;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public ReqRes register(ReqRes registrationRequest){
+        ReqRes resp=new ReqRes();
+
+    }
 }

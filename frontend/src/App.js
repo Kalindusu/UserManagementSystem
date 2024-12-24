a7b257d23b313c622421c9de1ec56fe8309ad93e
Kalindu Sudaraka <146382563+Kalindusu@users.noreply.github.com>
2024-12-24 23:16:40 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>This is a Applicatio Header</h1>
+        <h1>This is a Applicatio Header make new </h1>
       </header>
     </div>
   );
